4d13cedacede982ff4774a59502b8ff91af5c295
Gota7 <gotaseven@gmail.com>
2022-06-03 17:27:35 -0400

    Finish Tools Install Script
    It just works.

--- Lib/ht_common.py
+++ Lib/ht_common.py
@@ -6,17 +6,22 @@ import subprocess
 import zipfile
 
 # For calling an external program.
-def call_program(program: str):
+def call_program(program, dir = ""):
     if os.name != 'nt':
         program = "wine " + program         # Non windows OS use wine.
     arr = program.split(' ')
-    tmp_file = open("InstallFiles/tmp", "w")
+    tmp_file = open(os.path.join("InstallFiles", "tmp"), "w")
+    if dir != "":
+        curr_dir = os.path.curdir
+        os.chdir(dir)
     subprocess.call(arr, stdout=tmp_file)
+    if dir != "":
+        os.chdir(curr_dir)
     tmp_file.close()
 
 # Get temporary data.
 def get_tmp_data(read_mode):
-    tmp_file = open("InstallFiles/tmp", read_mode)
+    tmp_file = open(os.path.join("InstallFiles", "tmp"), read_mode)
     data = tmp_file.read()
     tmp_file.close()
     return data
@@ -36,8 +41,23 @@ def download_zip(url, subdir, dest):
     os.mkdir(dest)
     for zip_info in zip.infolist():
         if zip_info.filename.startswith(subdir):
-            zip_info.filename = zip_info.filename[len(subdir)+1:]
+            prefix = len(subdir) + 1
+            if prefix == 1:
+                prefix = 0
+            zip_info.filename = zip_info.filename[prefix:]
             if zip_info.filename != "":
                 #pathlib.Path(os.path.join(dest, zip_info.filename)).mkdir(parents=True, exist_ok=True)
                 zip.extract(zip_info, dest)
     zip.close()
+
+# If all the tools are installed.
+def tools_check():
+    if not os.path.exists(os.path.join("InstallFiles", "toolsInstalled")):
+        raise Exception("ERR: Tools are not installed!")
+    else:
+        print("Checking for tools installation... OK")
+
+# Run fireflower.
+def run_fireflower():
+    tools_check()
+    call_program("fireflower.exe", os.path.join("Tools", "Fireflower"))
--- toolsInstall.py
+++ toolsInstall.py
@@ -8,44 +8,54 @@ from io import SEEK_SET
 import os
 import shutil
 
-# Check for .NET 6.0 installation.
-ht_common.call_program("dotnet --list-sdks")
-if not "6.0" in ht_common.get_tmp_data("r"):
-    print("Checking .NET 6.0... FAIL")
-    print("ERR: .NET 6.0 is not installed and is needed to run Ndst!")
-    exit(0)
-print("Checking .NET 6.0... OK")
+def toolsInstall():
 
-# Download SM64DSe.
-if not os.path.exists("Editor") or ht_common.user_yn_prompt("SM64DSe is already installed, reinstall?"):
-    if os.path.exists("Editor"):
-        shutil.rmtree("Editor")
-    print("Installing SM64DSe...")
-    ht_common.download_zip("https://github.com/Gota7/SM64DSe-Ultimate/archive/refs/heads/master.zip", os.path.join("SM64DSe-Ultimate-master", "bin", "Release"), "Editor")
-    shutil.copyfile("InstallFiles/obj_list.txt", "Editor/obj_list.txt")
-    shutil.copyfile("InstallFiles/objectdb.xml", "Editor/objectdb.xml")
-print("SM64DSe installed... OK")
+    # Check for .NET 6.0 installation.
+    ht_common.call_program("dotnet --list-sdks")
+    if not "6.0" in ht_common.get_tmp_data("r"):
+        print("Checking .NET 6.0... FAIL")
+        print("ERR: .NET 6.0 is not installed and is needed to run Ndst!")
+        exit(0)
+    print("Checking .NET 6.0... OK")
 
-# Make tools directory.
-if not os.path.exists("Tools"):
-    os.mkdir("Tools")
+    # Download SM64DSe.
+    if not os.path.exists("Editor") or ht_common.user_yn_prompt("SM64DSe is already installed, reinstall?"):
+        if os.path.exists("Editor"):
+            shutil.rmtree("Editor")
+        print("Installing SM64DSe...")
+        ht_common.download_zip("https://github.com/Gota7/SM64DSe-Ultimate/archive/refs/heads/master.zip", os.path.join("SM64DSe-Ultimate-master", "bin", "Release"), "Editor")
+        shutil.copyfile(os.path.join("InstallFiles", "obj_list.txt"), os.path.join("Editor", "obj_list.txt"))
+        shutil.copyfile(os.path.join("InstallFiles", "objectdb.xml"), os.path.join("Editor", "objectdb.xml"))
+    print("SM64DSe installed... OK")
 
-# Download ARM_GCC.
-arm_gcc_path = os.path.join("Tools", "ARM_GCC")
-if not os.path.exists(arm_gcc_path) or ht_common.user_yn_prompt("ARM_GCC is already installed, reinstall?"):
-    if os.path.exists(arm_gcc_path):
-        shutil.rmtree(arm_gcc_path)
-    print("Installing ARM_GCC...")
-    ht_common.download_zip("https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-win32.zip", "gcc-arm-none-eabi-10.3-2021.10", arm_gcc_path)
-print("ARM_GCC installed... OK")
+    # Make tools directory.
+    if not os.path.exists("Tools"):
+        os.mkdir("Tools")
 
-# Download fireflower.
-# fireflower_path = os.path.join("Tools", "Fireflower")
-# if not os.path.exists(fireflower_path) or ht_common.user_yn_prompt("Fireflower is already installed, reinstall?"):
-#     if os.path.exists(fireflower_path):
-#         shutil.rmtree(fireflower_path)
-#     print("Installing Fireflower...")
-#     ht_common.download_zip("https://github.com/MammaMiaTeam/Fireflower/releases/download/1.0/fireflower.zip", "fireflower", fireflower_path)
-# print("Fireflower installed... OK")
+    # Download ARM_GCC.
+    arm_gcc_path = os.path.join("Tools", "ARM_GCC")
+    if not os.path.exists(arm_gcc_path) or ht_common.user_yn_prompt("ARM_GCC is already installed, reinstall?"):
+        if os.path.exists(arm_gcc_path):
+            shutil.rmtree(arm_gcc_path)
+        print("Installing ARM_GCC...")
+        ht_common.download_zip("https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-win32.zip", "gcc-arm-none-eabi-10.3-2021.10", arm_gcc_path)
+    print("ARM_GCC installed... OK")
 
-# Setup fireflower.
+    # Download fireflower.
+    fireflower_path = os.path.join("Tools", "Fireflower")
+    if not os.path.exists(fireflower_path) or ht_common.user_yn_prompt("Fireflower is already installed, reinstall?"):
+        if os.path.exists(fireflower_path):
+            shutil.rmtree(fireflower_path)
+        print("Installing Fireflower...")
+        ht_common.download_zip("https://github.com/MammaMiaTeam/Fireflower/releases/download/1.0/fireflower.zip", "", fireflower_path)
+        shutil.copyfile(os.path.join("InstallFiles", "buildroot.txt"), os.path.join("Tools", "Fireflower", "buildroot.txt"))
+    print("Fireflower installed... OK")
+
+    # Tools have been installed.
+    success = open(os.path.join("InstallFiles", "toolsInstalled"), "w")
+    success.close()
+    print("Tools have been successfully installed!")
+
+# Main method.
+if __name__ == "__main__":
+    toolsInstall()
